9b8909b7d213015ee944218156ed90b6687f4862
Aleksei Martishin <alekseim@amazon.com>
2020-10-04 03:39:02 +0200

    Move zeroes

---
+++ leetcode/MoveZeroes.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class MoveZeroes {
+    public static void moveZeroes(int[] nums) {
+        int idx = 0;
+
+        for (int n : nums) {
+            if (n != 0) {
+                nums[idx] = n;
+                ++idx;
+            } else {
+                nums[idx] = n;
+            }
+        }
+
+        for (int i = idx; i < nums.length; ++i) {
+            nums[i] = 0;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{1, 0, 1};
+        moveZeroes(nums);
+        System.out.println(Arrays.toString(nums));
+    }
+}
