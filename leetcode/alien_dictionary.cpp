15179a06d77180b1b33067506210aa241c5965fc
Aleksei Martishin <alekseim@amazon.com>
2020-12-30 04:12:00 +0100

    alien disctionary

--- leetcode/alien_dictionary.cpp
+++ leetcode/alien_dictionary.cpp
@@ -49,8 +49,7 @@ private:
         return result;
     }
 
-public:
-    string alienOrder(vector<string>& words) {
+    string alienOrderDFS(vector<string>& words) {
         if (words.size() == 1) {
             return words[0];
         }
@@ -83,5 +82,77 @@ public:
         }
 
         return topSort(charToPrevious);
+    }
+
+    string alienOrderBFS(vector<string>& words) {
+        if (words.size() == 1) {
+            return words[0];
+        }
+
+        unordered_map<char, unordered_set<char>> charToNext;
+        unordered_map<char, int> charToIncoming;
+
+        for (const auto& word : words) {
+            for (const auto& ch : word) {
+                charToNext[ch] = {};
+                charToIncoming[ch] = 0;
+            }
+        }
+
+        for (size_t i = 0; i < words.size() - 1; ++i) {
+            auto minLength = min(words[i].length(), words[i + 1].length());
+
+            bool isPrefix = true;
+
+            for (size_t j = 0; j < minLength; ++j) {
+                if (words[i][j] != words[i + 1][j]) {
+                    if (charToNext[words[i][j]].find(words[i + 1][j]) == charToNext[words[i][j]].end()) {
+                        charToNext[words[i][j]].insert(words[i + 1][j]);
+                        ++charToIncoming[words[i + 1][j]];
+                    }
+
+                    isPrefix = false;
+                    break;
+                }
+            }
+
+            if (isPrefix && words[i].length() > words[i + 1].length()) {
+                return "";
+            }
+        }
+
+        queue<char> charQueue;
+        for (const auto& pair : charToIncoming) {
+            if (pair.second == 0) {
+                charQueue.push(pair.first);
+            }
+        }
+
+        string order;
+
+        while (!charQueue.empty()) {
+            auto currentChar = charQueue.front();
+            charQueue.pop();
+
+            order += currentChar;
+
+            for (const auto& ch : charToNext[currentChar]) {
+                --charToIncoming[ch];
+                if (charToIncoming[ch] == 0) {
+                    charQueue.push(ch);
+                }
+            }
+        }
+
+        if (order.length() < charToIncoming.size()) {
+            return "";
+        }
+
+        return order;
+    }
+
+public:
+    string alienOrder(vector<string>& words) {
+        return alienOrderBFS(words);
     }
 };
