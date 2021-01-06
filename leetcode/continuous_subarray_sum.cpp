a599e1977a72027a6e812c8a3a2123433d99ad1a
Aleksei Martishin <alekseim@amazon.com>
2021-01-06 16:23:51 +0100

    continuous_subarray_sum

--- leetcode/continuous_subarray_sum.cpp
+++ leetcode/continuous_subarray_sum.cpp
@@ -1,60 +1,58 @@
-#include <iostream>
-#include <string>
-#include <vector>
-#include <algorithm>
-#include <numeric>
-#include <cmath>
-#include <queue>
-#include <stack>
-#include <map>
-#include <set>
-#include <unordered_map>
-#include <unordered_set>
-#include <limits>
-#include <cassert>
-#include <fstream>
-#include <array>
+class Solution {
+private:
+    bool checkSubarraySumMap(vector<int>& nums, int k) {
+        unordered_map<int, int> sumsToIdx;
+        k = abs(k);
 
-using namespace std;
+        int sum  = 0;
+        sumsToIdx[0] = -1;
 
-#define endl '\n'
+        for (size_t i = 0; i < nums.size(); ++i) {
+            sum += nums[i];
 
-bool checkSubarraySum(const vector<int>& nums, int k) {
-    vector<int> prefixSums{0};
+            if (k != 0) {
+                sum %= k;
+            }
 
-    int currentSum = 0;
-
-    for (auto n : nums) {
-        currentSum += n;
-        prefixSums.push_back(currentSum);
-    }
-
-//    for (auto p: prefixSums) {
-//        cout << p << ' ';
-//    }
-//    cout << endl;
-
-    for (size_t i = 0; i <= nums.size(); ++i) {
-        for (size_t j = i + 2; j <= nums.size(); ++j) {
-            currentSum = prefixSums[j] - prefixSums[i];
-
-            if (k != 0 && currentSum >= k && currentSum % k == 0) {
-                return true;
-            } else if (currentSum == 0) {
-                return true;
+            auto found = sumsToIdx.find(sum);
+            if (found != sumsToIdx.end()) {
+                if (i - found->second > 1) {
+                    return true;
+                }
+            } else {
+                sumsToIdx[sum] = i;
             }
         }
+
+        return false;
     }
 
-    return false;
-}
+    bool checkSubarraySumPrefix(const vector<int>& nums, int k) {
+        vector<int> prefixSums{0};
 
-int main() {
-    std::ios_base::sync_with_stdio(0);
-    cin.tie(0);
-    cout.precision(10);
+        int currentSum = 0;
 
-    cout << checkSubarraySum({0, 0}, 0);
+        for (auto n : nums) {
+            currentSum += n;
+            prefixSums.push_back(currentSum);
+        }
 
-    return 0;
-}
+        for (size_t i = 0; i <= nums.size(); ++i) {
+            for (size_t j = i + 2; j <= nums.size(); ++j) {
+                currentSum = prefixSums[j] - prefixSums[i];
+
+                if (k != 0 && currentSum >= k && currentSum % k == 0) {
+                    return true;
+                } else if (currentSum == 0) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+public:
+    bool checkSubarraySum(vector<int>& nums, int k) {
+        return checkSubarraySumMap(nums, k);
+    }
+};
