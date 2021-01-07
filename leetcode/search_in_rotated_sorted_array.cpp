f6aa5a64355df87a466fdb708d61617996b2a60c
Aleksei Martishin <alekseim@amazon.com>
2021-01-07 22:26:20 +0100

    search_in_rotated_sorted_array

--- leetcode/search_in_rotated_sorted_array.cpp
+++ leetcode/search_in_rotated_sorted_array.cpp
@@ -1,84 +1,90 @@
-#include <iostream>
-#include <string>
-#include <vector>
-#include <algorithm>
-#include <cmath>
-#include <queue>
-#include <stack>
-#include <map>
-#include <set>
-#include <unordered_map>
-#include <unordered_set>
-#include <limits>
-#include <cassert>
-#include <fstream>
-#include <array>
-
-using namespace std;
-
-#define endl '\n'
-
-
 class Solution {
-public:
-    int get_shift(vector<int> &nums) {
+private:
+    int findShift(vector<int>& nums) {
         int l = 0;
         int r = nums.size() - 1;
 
-        while (l < r) {
-            int m = (l + r) / 2;
-
-            if (nums[m] > nums[r])
-                l = m + 1;
-            else
-                r = m;
+        if (nums[l] < nums[r]) {
+            return 0;
         }
 
-        return r;
+        while (l <= r) {
+            int m = (l + r) / 2;
+            cout << l << ' ' << r << ' ' << m << endl;
+
+            if (nums[m] > nums[m + 1]) {
+                return m + 1;
+            }
+
+            if (nums[m] < nums[l]) {
+                r = m - 1;
+            } else {
+                l = m + 1;
+            }
+        }
+
+        return 0;
     }
 
-    int search(vector<int> &nums, int target) {
-        auto shift = get_shift(nums);
+    int searchWithShift(vector<int>& nums, int target) {
+        if (nums.size() == 1) {
+            return nums[0] == target ? 0 : -1;
+        }
 
-        int size = nums.size();
+        int shift = findShift(nums);
+        cout << shift << endl;
 
         int l = 0;
         int r = nums.size();
 
         while (l < r) {
             int m = (l + r) / 2;
-            auto mShifted = m + shift;
+            int mShifted = m + shift;
 
-            if (mShifted >= size)
-                mShifted = mShifted - size;
+            if (mShifted >= nums.size()) {
+                mShifted -= nums.size();
+            }
 
-            if (nums[mShifted] == target)
+            if (nums[mShifted] == target) {
                 return mShifted;
-            else if (nums[mShifted] > target)
+            } else if (nums[mShifted] > target) {
                 r = m;
-            else
+            } else {
                 l = m + 1;
+            }
         }
 
         return -1;
     }
+
+    int searchOnePass(vector<int>& nums, int target) {
+        int l = 0;
+        int r = nums.size() - 1;
+
+        while (l <= r) {
+            int m = (l + r) / 2;
+
+            if (nums[m] == target) {
+                return m;
+            } else if (nums[m] >= nums[l]) {
+                if (target >= nums[l] && target < nums[m]) {
+                    r = m - 1;
+                } else {
+                    l = m + 1;
+                }
+            } else {
+                if (target <= nums[r] && target > nums[m]) {
+                    l = m + 1;
+                } else {
+                    r = m - 1;
+                }
+            }
+        }
+
+        return -1;
+    }
+public:
+    int search(vector<int>& nums, int target) {
+        return searchOnePass(nums, target);
+    }
 };
-
-
-int main() {
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
-    cout.precision(10);
-
-    int n, target;
-    cin >> n >> target;
-
-    vector<int> nums(n);
-
-    for (size_t i = 0; i < n; ++i)
-        cin >> nums[i];
-
-    cout << Solution().search(nums, target) << endl;
-
-    return 0;
-}
