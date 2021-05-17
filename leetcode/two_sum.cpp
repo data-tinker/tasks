464bfef75fe7170643905926b4a1f97658919835
Aleksei Martishin <alekseim@amazon.com>
2021-05-18 01:48:54 +0200

    two_sum

--- leetcode/two_sum.cpp
+++ leetcode/two_sum.cpp
@@ -20,6 +20,33 @@ vector<int> twoSum(vector<int>& nums, int target) {
     return {-1, -1};
 }
 
+vector<int> twoSumIterators(vector<int>& nums, int target) {
+        vector<pair<int, int>> numsWithIndices;
+
+        for (int i = 0; i < nums.size(); ++i) {
+            numsWithIndices.emplace_back(nums[i], i);
+        }
+
+        sort(begin(numsWithIndices), end(numsWithIndices));
+
+        int l = 0;
+        int r = nums.size() - 1;
+
+        while (l < r) {
+            int sum = numsWithIndices[l].first + numsWithIndices[r].first;
+
+            if (sum == target) {
+                return {numsWithIndices[l].second, numsWithIndices[r].second};
+            } else if (sum < target) {
+                ++l;
+            } else {
+                --r;
+            }
+        }
+
+        return {-1, -1};
+    }
+
 int main() {
     vector<int> v{3, 2, 4};
 
